44a0f42c07c24e9c55a0428e2ab7866dd7f94c7c
minorin2847 <minorin2847@gmail.com>
2025-08-26 18:34:00 +0700

    Initialize both backend and frontend, add example hello world

---
+++ Backend/demo/src/main/java/com/example/demo/controller/GreetingController.java
@@ -0,0 +1,22 @@
+package com.example.demo.controller;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.model.Greeting;
+
+@RestController
+public class GreetingController {
+    
+    private static final String template = "Hello %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/greetings")
+    public Greeting Greetings(@RequestParam(defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+    
+}
---
+++ Frontend/itjob/src/App.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+function App() {
+  const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+  const [name, setName] = useState("");
+  const [hello, setHello] = useState("Hello world!");
+  const getHelloFromName = async (name: string) => {
+    const response = await axios.get(`${BACKEND_URL}/greetings?name=${name}`);
+    setHello(response.data.content)
+  }
+
+  const handleInputEnter = async (event) => {
+    if (event.key === "Enter") {
+      await getHelloFromName(name)
+    }
+  }
+  return (
+    <>
+      <div className="flex flex-col justify-center items-center space-y-5  w-screen h-screen">
+        <p className='text-6xl text-blue-700'>{hello}</p>
+        <div className="flex flex-row space-x-3">
+          <input className="border text-blue-500 text-2xl" name="HelloWorldInput" onChange={e => setName(e.target.value)} onKeyDown={async e => await handleInputEnter(e)}></input>
+          <button className="border-blue-500 border w-24" onClick={async () => await getHelloFromName(name)}>Change your name</button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
