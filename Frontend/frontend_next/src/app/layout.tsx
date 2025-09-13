45cf0d5a8cc950ea5c10e5c865ee860e9b9adf98
FireFish <truongboboy2005@gmail.com>
2025-09-13 16:26:23 +0700

    create footer

--- Frontend/frontend_next/src/app/layout.tsx
+++ Frontend/frontend_next/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Header } from "./components/header/Header";
+import Footer from "./components/footer/Footer";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -18,7 +19,7 @@ export default function RootLayout({
       <body>
         <Header/>
         {children}
-
+        <Footer/>
       </body>
     </html>
   );
