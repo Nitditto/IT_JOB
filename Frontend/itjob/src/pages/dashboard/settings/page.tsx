43de2c1c66e8d60fecc4222572f3b8c3b72cb3a9
minorin2847 <minorin2847@gmail.com>
2025-11-23 02:29:25 +0700

    [FRONTEND] style changes for /dashboard/settings

--- Frontend/itjob/src/pages/dashboard/settings/page.tsx
+++ Frontend/itjob/src/pages/dashboard/settings/page.tsx
@@ -1,7 +1,6 @@
-import { Info, UserStar } from 'lucide-react';
+import { KeyRound, Pen, UserRoundX } from 'lucide-react';
 import React, { useEffect } from 'react'
 import {Link} from 'react-router'
-import { PiUserCircleGearFill, PiUserCirclePlusBold } from "react-icons/pi";
 import { useAuth } from '@/context/AuthContext';
 const Settings = () => {
   const { user } = useAuth();
@@ -12,7 +11,7 @@ const Settings = () => {
     },[])
     return (
       <>
-        <div className=" pt-[30px] pb-[60px]">
+        <div className=" pt-[30px] pb-[60px] h-screen">
           <div className="container">
             <h2 className="font-semibold text-3xl">
               Cài đặt chung 
@@ -21,18 +20,18 @@ const Settings = () => {
               <Link 
               to={isCompany ? "/dashboard/settings/company-profile" : "/dashboard/settings/user-profile"}
               className="font-medium rounded-md cursor-pointer bg-gradient-to-tr from-indigo-200 to-indigo-100 text-gray-600 flex items-center justify-between gap-2 px-6 py-5 text-2xl">
-                <Info className='' size={30}/>
+                <Pen className='' size={30}/>
                 <span className="">
-                  {isCompany ? "Thông tin công ty" : "Thông tin cá nhân"}
+                  {"Chỉnh sửa thông tin"}
                 </span>
               </Link>
               <Link to="#" className="font-medium rounded-md cursor-pointer bg-gradient-to-tr from-indigo-200 to-indigo-100 text-gray-600 flex items-center justify-between gap-2 px-6 py-5 text-2xl">
-                <PiUserCircleGearFill className='' size={30}/>
+                <KeyRound className='' size={30}/>
                 <Link to="/dashboard/settings/change-password" className="">Đổi mật khẩu</Link>
               </Link>
               
               <Link to="#" className="font-medium rounded-md cursor-pointer bg-gradient-to-tr from-indigo-200 to-indigo-100 text-gray-600 flex items-center justify-between gap-2 px-6 py-5 text-2xl">
-                <PiUserCirclePlusBold className='' size={30}/>
+                <UserRoundX className='' size={30}/>
                 <Link to={"#"} className="">Xóa tài khoản</Link>
               </Link>
             </div>
